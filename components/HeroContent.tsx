2a4cbc7933dcbe3ad557c0acb962af5458a3b886
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2025-10-22 23:34:28 +0300

    add underline word component

--- components/HeroContent.tsx
+++ components/HeroContent.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import Label from "./Label";
 import Button from "./Button";
+import Image from "next/image";
+import UnderlinedWord from "./UnderlinedWord";
 
 export default function HeroContent() {
   return (
     <>
       <Label>Secure your data</Label>
-      <p className="text-gradient-pink-blue font-sora mt-1.5 text-[clamp(30px,5vw,48px)] leading-[120%] font-semibold">
-        Identity-hub is a better way to
-        <br className="hidden sm:block" /> achieve privacy
-      </p>
+      <div className="text-gradient-pink-blue font-sora relative mt-1.5 text-[clamp(30px,5vw,48px)] leading-[120%] font-semibold">
+        <p>
+          Identity-hub is a better way to
+          <br className="hidden sm:block" /> achieve{" "}
+          <UnderlinedWord>privacy</UnderlinedWord>
+        </p>
+      </div>
       <p className="font-Aeonik mt-5 text-[clamp(14px,2vw,20px)]">
         Make your data invisible by generating unlimited identities. The
         next-level in <br className="hidden sm:block" /> privacy protection for
---
+++ components/UnderlinedWord.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+
+export default function UnderlinedWord({ children }: { children: string }) {
+  const wordRef = useRef<HTMLSpanElement | null>(null);
+  const underlineRef = useRef<HTMLSpanElement | null>(null);
+
+  function underlinePosition() {
+    const wordEl = wordRef.current;
+    const underlineEl = underlineRef.current;
+
+    if (!wordEl || !underlineEl) return;
+
+    const rect = wordEl.getBoundingClientRect();
+    const parentRect = wordEl.parentElement?.getBoundingClientRect();
+
+    if (!rect || !parentRect) return;
+    underlineEl.style.width = `${rect.width + rect.width * 0.16}px`;
+
+    const leftOffset = (underlineEl.clientWidth - rect.width) / 2;
+
+    const left = rect.left - parentRect.left - leftOffset;
+    const top = rect.top - parentRect.top + rect.height - 6;
+
+    underlineEl.style.left = `${left}px`;
+    underlineEl.style.top = `${top}px`;
+  }
+
+  useEffect(() => {
+    underlinePosition();
+    window.addEventListener("resize", underlinePosition);
+    return () => window.removeEventListener("resize", underlinePosition);
+  }, []);
+
+  return (
+    <>
+      <span ref={wordRef}>{children}</span>
+
+      <span className="absolute h-3" ref={underlineRef}>
+        <Image src={"/underline-word.svg"} fill alt="underline-word" />
+      </span>
+    </>
+  );
+}
