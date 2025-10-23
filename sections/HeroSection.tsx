b6609ea3927091f098d217427bdb6d257554e053
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2025-10-23 09:28:58 +0300

    fix herosection bug

--- sections/HeroSection.tsx
+++ sections/HeroSection.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section className="relative min-h-screen overflow-x-hidden">
+    <section className="relative">
       <BackgroundLights />
       <Image
         src={"/radio-waves.svg"}
@@ -15,11 +15,11 @@ export default function HeroSection() {
         alt="radio-waves BG"
         className="!top-1/2 !left-1/2 -z-10 aspect-square !h-auto !w-[90%] -translate-1/2 object-cover lg:!top-0 lg:!h-full lg:min-h-[998px] lg:!w-full lg:translate-y-0"
       />
+      <FloatingBalls />
       <div className="mx-auto max-w-[794px] px-6 pt-[6%] text-center lg:pt-[66px]">
         <HeroContent />
         <HeroVisual />
       </div>
-      <FloatingBalls />
     </section>
   );
 }
