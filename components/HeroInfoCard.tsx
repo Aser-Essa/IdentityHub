f7da205944989583cd843e1eb378310aa6c4fa25
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2026-06-27 20:33:06 +0300

    Fix animations on mobile

--- components/HeroInfoCard.tsx
+++ components/HeroInfoCard.tsx
@@ -6,7 +6,7 @@ import { useMediaQuery } from "react-responsive";
 type HeroInfoCardProps = {
   left?: number;
   right?: number;
-  top?: number;
+  y?: number;
   bottom?: number;
   iconSrc: string;
   title: string;
@@ -16,7 +16,7 @@ type HeroInfoCardProps = {
 export default function HeroInfoCard({
   left,
   right,
-  top,
+  y,
   bottom,
   iconSrc,
   title,
@@ -29,7 +29,7 @@ export default function HeroInfoCard({
     setIsMounted(true);
   }, []);
 
-  const topPercent = top && isMounted && isMobile ? top / 2 : top;
+  const topPercent = y && isMounted && isMobile ? y / 2 : y;
   const bottomPercent = bottom && isMounted && isMobile ? bottom * 2 : bottom;
 
   const positons = {
--- components/HeroInfoCards.tsx
+++ components/HeroInfoCards.tsx
@@ -5,12 +5,12 @@ export default function HeroInfoCards() {
   return (
     <div className=" ">
       {HeroInfoCardsDetails.map(
-        ({ left, right, top, bottom, iconSrc, title, description }, idx) => (
+        ({ left, right, y, bottom, iconSrc, title, description }, idx) => (
           <HeroInfoCard
             key={`${title} , ${idx}`}
             left={left}
             right={right}
-            top={top}
+            y={y}
             bottom={bottom}
             iconSrc={iconSrc}
             title={title}
