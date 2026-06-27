3d0de82e65b3ee39ccc63984507e5648c2342242
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2026-06-28 01:41:20 +0300

    Fix animations on mobile

--- components/GSAPClientWrapper.tsx
+++ components/GSAPClientWrapper.tsx
@@ -18,9 +18,7 @@ export default function GSAPClientWrapper({
   useGSAP(() => {
     ScrollSmoother.create({
       smooth: 1.5,
-      smoothTouch: 0,
       effects: true,
-      normalizeScroll: true,
     });
   }, []);
 
