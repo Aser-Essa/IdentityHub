7e7050cc653f957ab2822f10ae882d0e9e2b89d1
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2026-06-28 01:13:44 +0300

    Fix animations on mobile

--- components/GSAPClientWrapper.tsx
+++ components/GSAPClientWrapper.tsx
@@ -16,8 +16,18 @@ export default function GSAPClientWrapper({
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin, SplitText);
 
   useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 1.5,
+      smoothTouch: 0.001,
+      effects: true,
+    });
+  }, []);
+
+  useGSAP(() => {
     gsap.to(".GSAP-client-wrapper", {
-      opacity: 100,
+      opacity: 1,
+      duration: 0.6,
+      ease: "power2.out",
     });
   });
 
