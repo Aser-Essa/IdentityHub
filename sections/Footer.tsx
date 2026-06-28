5b14b9dd0496b2aa17e2864f3c2b637c3bf31b8e
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2026-06-28 14:28:47 +0300

    Fix animations on mobile

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -3,7 +3,7 @@ import LogoAndFooterLinks from "../components/LogoAndFooterLinks";
 
 export default function Footer() {
   return (
-    <footer className="relative h-73.75 md:min-h-62.5">
+    <footer className="relative min-h-62.5">
       <div className="w-full space-y-5 p-4 pb-5 sm:p-8 flex flex-col">
         <LogoAndFooterLinks />
         <CopywriteAndSocialLinks />
